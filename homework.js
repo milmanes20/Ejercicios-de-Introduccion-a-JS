eb4682ff1eafd3386b2ecc4ad8c76639ef1cae22
Fabio D. Argañaraz A. <ing.fabio.arg@gmail.com>
2025-05-07 23:32:09 -0300

    Create homework.js

---
+++ homework.js
@@ -0,0 +1,236 @@
+// En estas primeras 6 preguntas, reemplaza `null` por la respuesta
+
+// Crea una variable "string", puede contener lo que quieras:
+const nuevaString = null;
+
+// Crea una variable numérica, puede ser cualquier número:
+const nuevoNum = null;
+
+// Crea una variable booleana:
+const nuevoBool = null;
+
+// Resuelve el siguiente problema matemático:
+const nuevaResta = 10 - null === 5;
+
+// Resuelve el siguiente problema matemático:
+const nuevaMultiplicacion = 10 * null === 40 ;
+
+// Resuelve el siguiente problema matemático:
+const nuevoModulo = 21 % 5 === null;
+
+// En los próximos 22 problemas, deberás completar la función.
+// Todo tu código irá dentro de las llaves de la función.
+// Asegúrate que usas "return" cuando la consola te lo pida.
+// Pista: "console.log()" NO fucionará.
+// No cambies los nombres de las funciones.
+
+function devolverString(str) {
+  // "Return" la string provista: str
+  // Tu código:
+}
+
+function suma(x, y) {
+  // "x" e "y" son números
+  // Suma "x" e "y" juntos y devuelve el valor
+  // Tu código:
+}
+
+function resta(x, y) {
+  // Resta "x" de "y" y devuelve el valor
+  // Tu código:
+}
+
+function multiplica(x, y) {
+  // Multiplica "x" por "y" y devuelve el valor
+  // Tu código:
+}
+
+function divide(x, y) {
+  // Divide "x" entre "y" y devuelve el valor
+  // Tu código:
+}
+
+function sonIguales(x, y) {
+  // Devuelve "true" si "x" e "y" son iguales
+  // De lo contrario, devuelve "false"
+  // Tu código:
+}
+
+function tienenMismaLongitud(str1, str2) {
+  // Devuelve "true" si las dos strings tienen la misma longitud
+  // De lo contrario, devuelve "false"
+  // Tu código:
+}
+
+function menosQueNoventa(num) {
+  // Devuelve "true" si el argumento de la función "num" es menor que noventa
+  // De lo contrario, devuelve "false"
+  // Tu código:
+}
+
+function mayorQueCincuenta(num) {
+  // Devuelve "true" si el argumento de la función "num" es mayor que cincuenta
+  // De lo contrario, devuelve "false"
+  // Tu código:
+}
+
+function obtenerResto(x, y) {
+  // Obten el resto de la división de "x" entre "y"
+  // Tu código:
+}
+
+function esPar(num) {
+  // Devuelve "true" si "num" es par
+  // De lo contrario, devuelve "false"
+  // Tu código:
+}
+
+function esImpar(num) {
+  // Devuelve "true" si "num" es impar
+  // De lo contrario, devuelve "false"
+  // Tu código:
+}
+
+function elevarAlCuadrado(num) {
+  // Devuelve el valor de "num" elevado al cuadrado
+  // ojo: No es raiz cuadrada!
+  // Tu código:
+}
+
+function elevarAlCubo(num) {
+  // Devuelve el valor de "num" elevado al cubo
+  // Tu código:
+}
+
+function elevar(num, exponent) {
+  // Devuelve el valor de "num" elevado al exponente dado en "exponent"
+  // Tu código:
+}
+
+function redondearNumero(num) {
+  // Redondea "num" y devuélvelo
+  // Tu código:
+}
+
+function redondearHaciaArriba(num) {
+  // Redondea "num" hacia arriba y devuélvelo
+  // Tu código:
+}
+
+function agregarSimboloExclamacion(str) {
+  // Agrega un símbolo de exclamación al final de la string "str" y devuelve una nueva string
+  // Ejemplo: "hello world" pasaría a ser "hello world!"
+  // Tu código:
+}
+
+function combinarNombres(nombre, apellido) {
+  // Devuelve "nombre" y "apellido" combinados en una string y separados por un espacio.
+  // Ejemplo: "Soy", "Pablo" -> "Soy Pablo"
+  // Tu código:
+}
+
+function obtenerSaludo(nombre) {
+  // Toma la string "nombre" y concatena otras string en la cadena para que tome la siguiente forma:
+  // "Martin" -> "Hola Martin!"
+  // Tu código:
+}
+
+function obtenerAreaRectangulo(alto, ancho) {
+  // Retornar el area de un cuadrado teniendo su altura y ancho
+  // Tu código:
+}
+
+// No cambies los nombres de las funciones.
+
+function obtenerMayor(x, y) {
+  // "x" e "y" son números enteros (int).
+  // Devuelve el número más grande
+  // Si son iguales, devuelve cualquiera de los dos
+  // Tu código:
+}
+
+function saludo(idioma) {
+  // Devuelve un saludo en tres diferentes lenguajes:
+  // Si "idioma" es "aleman", devuelve "Guten Tag!"
+  // Si "idioma" es "mandarin", devuelve "Ni Hao!"
+  // Si "idioma" es "ingles", devuelve "Hello!"
+  // Si "idioma" no es ninguno de los anteiores o es `undefined` devuelve "Hola!"
+  // Tu código:
+}
+
+function esDiezOCinco(numero) {
+    // Devuelve "true" si "numero" es 10 o 5
+    // De lo contrario, devuelve "false"
+    // Tu código:
+}
+
+function estaEnRango(numero) {
+  // Devuelve "true" si "numero" es menor que 50 y mayor que 20
+  // De lo contrario, devuelve "false"
+  // Tu código:
+}
+
+function esEntero(numero) {
+  // Devuelve "true" si "numero" es un entero (int/integer)
+  // Ejemplo: 0.8 -> false
+  // Ejemplo: 1 -> true
+  // Ejemplo: -10 -> true
+  // De lo contrario, devuelve "false"
+  // Pista: Puedes resolver esto usando `Math.floor`
+  // Tu código:
+}
+
+function fizzBuzz(numero) {
+  // Si "numero" es divisible entre 3, devuelve "fizz"
+  // Si "numero" es divisible entre 5, devuelve "buzz"
+  // Si "numero" es divisible entre 3 y 5 (ambos), devuelve "fizzbuzz"
+  // De lo contrario, devuelve el numero
+}
+
+function esPrimo(numero) {
+  // Devuelve "true" si "numero" es primo
+  // De lo contrario devuelve "falso"
+  // Pista: un número primo solo es divisible por sí mismo y por 1
+  // Pista 2: Puedes resolverlo usando un bucle `for`
+  // Nota: Los números 0 y 1 NO son considerados números primos
+}
+
+// No modificar nada debajo de esta línea
+// --------------------------------
+
+module.exports = {
+  nuevaString,
+  nuevoNum,
+  nuevoBool,
+  nuevaResta,
+  nuevaMultiplicacion,
+  nuevoModulo,
+  devolverString,
+  tienenMismaLongitud,
+  sonIguales,
+  menosQueNoventa,
+  mayorQueCincuenta,
+  suma,
+  resta,
+  divide,
+  multiplica,
+  obtenerResto,
+  esPar,
+  esImpar,
+  elevarAlCuadrado,
+  elevarAlCubo,
+  elevar,
+  redondearNumero,
+  redondearHaciaArriba,
+  agregarSimboloExclamacion,
+  combinarNombres,
+  obtenerSaludo,
+  obtenerAreaRectangulo,
+  obtenerMayor,
+  saludo,
+  esDiezOCinco,
+  estaEnRango,
+  esEntero,
+  fizzBuzz,
+  esPrimo
+};
